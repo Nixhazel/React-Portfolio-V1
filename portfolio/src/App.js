0f1ea2e0b6e65563fa7a4eccd0ea124339eb78dd
Nixhazel <neolokor1@gmail.com>
2023-03-04 06:44:41 +0100

    added assets and started header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
