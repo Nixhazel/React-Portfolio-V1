27691cc6597489f00e6f4de995f3e917f5eff2e5
Nixhazel <neolokor1@gmail.com>
2023-03-05 03:28:07 +0100

    completed nav on small creens

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+	/*=============== Toggle menu ===============*/
+	const [Toggle, showMenu] = useState(false);
+
 	return (
 		<header className='header'>
 			<nav className='nav container'>
 				<a href='index.html' className='nav_logo'>
-					Smith
+					Olokor
 				</a>
-				<div className='nav__menu'>
+				<div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
 					<ul className='nav__list grid'>
 						<li className='nav__item'>
 							<a href='#home' className='nav__link active-link'>
@@ -41,9 +44,11 @@ const Header = () => {
 							</a>
 						</li>
 					</ul>
-					<i class='uil uil-times nav__close'></i>
+					<i
+						class='uil uil-times nav__close'
+						onClick={() => showMenu(!Toggle)}></i>
 				</div>
-				<div className='nav__toggle'>
+				<div className='nav__toggle' onClick={() => showMenu(!Toggle)}>
 					<i class='uil uil-apps'></i>
 				</div>
 			</nav>
