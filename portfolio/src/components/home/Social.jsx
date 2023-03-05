7c0a6656f004ed2f0efa7e7797dc758f7dc7537c
Nixhazel <neolokor1@gmail.com>
2023-03-05 03:58:44 +0100

    Started Home Section

---
+++ portfolio/src/components/home/Social.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Social = () => {
+	return (
+		<div className='home__social'>
+			<a
+				href='https://www.instagram.com/nixhazel/'
+				className='home__social-icon'
+				target='_blank'>
+				<i class='uil uil-instagram'></i>
+			</a>
+
+			<a
+				href='https://www.linkedin.com/in/emmanuel-olokor-56b260244/'
+				className='home__social-icon'
+				target='_blank'>
+				<i class='uil uil-linkedin-alt'></i>
+			</a>
+
+			<a
+				href='https://github.com/Nixhazel'
+				className='home__social-icon'
+				target='_blank'>
+				<i class='uil uil-github-alt'></i>
+			</a>
+		</div>
+	);
+};
+
+export default Social;
