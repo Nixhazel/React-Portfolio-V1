d018eb46372e530284fc79873214449d7144b04e
Nixhazel <neolokor1@gmail.com>
2023-03-08 07:06:14 +0100

    started Skills section

---
+++ portfolio/src/components/skills/Backend.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const Backend = () => {
+	return (
+		<div className='skills__content'>
+			<h3 className='skills__title'>Backend Engineer</h3>
+
+			<div className='skills__box'>
+				<div className='skills__group'>
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>Node Js</h3>
+							<span className='skills__level'>Intermediate</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>Express</h3>
+							<span className='skills__level'>Intermediate</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>GraphQL</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>MySql</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+				</div>
+
+				<div className='skills__group'>
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>MongoDB/Mongoose</h3>
+							<span className='skills__level'>Intermediate</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>Git & GitHub</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>Docker</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i class='bx bx-badge-check'></i>
+
+						<div>
+							<h3 className='skills__name'>CI/CD</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Backend;
