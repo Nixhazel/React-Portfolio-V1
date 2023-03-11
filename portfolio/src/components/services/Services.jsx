139e596094bd988833811e75952e27c3978374d5
Nixhazel <neolokor1@gmail.com>
2023-03-11 17:25:49 +0100

    started services

---
+++ portfolio/src/components/services/Services.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Services = () => {
+	return (
+		<section className='services section' id='services'>
+			<h2 className='section__title'>Services</h2>
+            <span className='section__subtitle'>What i offer</span>
+            
+            <div className="services__container container grid">
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil services__icon"></i>
+                    </div>
+                </div>
+            </div>
+		</section>
+	);
+};
+
+export default Services;
