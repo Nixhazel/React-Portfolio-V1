1d6225e05160646c04a061425106443e57eb72dd
Nixhazel <neolokor1@gmail.com>
2023-03-22 01:14:46 +0100

    personalising

--- portfolio/src/components/work/Data.jsx
+++ portfolio/src/components/work/Data.jsx
@@ -1,43 +1,59 @@
-import Work1 from '../../assets/work1.jpg'
-import Work2 from '../../assets/work2.jpg'
-import Work3 from '../../assets/work3.jpg'
-import Work4 from '../../assets/work4.jpg'
-import Work5 from '../../assets/work5.jpg'
+import Work1 from '../../assets/work1.png'
+import Work2 from '../../assets/work2.png'
+import Work3 from '../../assets/work3.png'
+import Work4 from '../../assets/work4.png'
+import Work5 from '../../assets/work5.png'
+import Work6 from '../../assets/work6.png'
+import Work7 from '../../assets/work7.png'
 
 export const projectsData = [
 	{
 		id: 1,
 		image: Work1,
-		title: "Web design",
-		category: "web",
-		link: "https",
+		title: "Simon Game",
+		category: "Front-End",
+		link: "https://nixhazel-simon-game.netlify.app/",
 	},
 	{
 		id: 2,
 		image: Work2,
-		title: "App movil",
-		category: "app",
-		link: "https",
+		title: "Star-Wars Characters API",
+		category: "Front-End",
+		link: "https://olokor-starwars-api-challenge.netlify.app/",
 	},
 	{
 		id: 3,
 		image: Work3,
-		title: "Brand design",
-		category: "design",
-		link: "https",
+		title: "Todo List App",
+		category: "Front-End",
+		link: "https://olokor-todo-app.netlify.app/",
 	},
 	{
 		id: 4,
 		image: Work4,
-		title: "App movil",
-		category: "app",
-		link: "https",
+		title: "Doctor Apontment Booking App",
+		category: "Full-stack",
+		link: "https4",
 	},
 	{
 		id: 5,
 		image: Work5,
-		title: "Web design",
-		category: "web",
+		title: "E-Move",
+		category: "Full-stack",
+		link: "https",
+	},
+	{
+		id: 6,
+		image: Work6,
+		title: "Portfolio V0.1",
+		category: "Front-End",
+		link: "https://nixhazel.github.io/Personal-Profile/",
+	},
+	{
+		id: 7,
+		image: Work7,
+		title: "Library App",
+		category: "Full-stack",
 		link: "https",
 	},
 ];
@@ -47,12 +63,12 @@ export const projectsNav = [
 		name: "all",
 	},
 	{
-		name: "web",
+		name: "Front-End",
 	},
+	// {
+	// 	name: "Back-End",
+	// },
 	{
-		name: "app",
-	},
-	{
-		name: "design",
+		name: "Full-stack",
 	}
 ];
