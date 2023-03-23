448405949c7e2517814fbccf93c8c32110bf1879
Nixhazel <neolokor1@gmail.com>
2023-03-23 14:20:01 +0100

    fixed warnings in scroll up

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -12,7 +12,7 @@ const ScrollUp = () => {
         }
     })
 	return (
-		<a href='#' className='scrollup'>
+		<a href='#home' className='scrollup'>
 			<i className='uil uil-arrow-up scrollup__icon'></i>
 		</a>
 	);
